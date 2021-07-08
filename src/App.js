ac64b0941716ed385d94262e722b6c6a4c752163
Shruti Verma <shrutiverma3105@gmail.com>
2021-07-08 14:08:48 +0530

    complete addressbook code

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import AddressBookForm from "./components/address-book-form/address-book-form";
+import HomePage from "./components/address-book-home/address-book-home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/address-book-form">
+            <AddressBookForm />
+          </Route>
+          <Route exact path="/home">
+            <HomePage />
+          </Route>
+          <Route exact path="/address-book-form/:id">
+            <AddressBookForm />
+          </Route>
+          <Route exact path="">
+            <Redirect exact from="/" to="/home" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  baseUrl: "http://localhost:8080/",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+};
